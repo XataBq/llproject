f9ec5efe5448c17b1228a899cae74b5b2f188e17
XataBq <73535824+XataBq@users.noreply.github.com>
2022-12-28 15:11:01 +0300

    Update first_run.bat

--- first_run.bat
+++ first_run.bat
@@ -1,7 +1,7 @@
 call python -m venv venv
 call venv\Scripts\activate
-pip install Django Pillow django-ckeditor
-rem cd polyakov
-rem python manage.py runserver 127.0.0.1:8000
-rem start msedge http://127.0.0.1:8000/
+pip install random2 Django Pillow django-ckeditor
+cd polyakov
+python manage.py runserver 127.0.0.1:8000
+start msedge http://127.0.0.1:8000/
 pause
